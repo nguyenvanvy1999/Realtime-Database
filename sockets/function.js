87e01c2671fbeac0ef7c9a6792748733d8c5a194
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2020-12-24 12:09:00 +0700

    Edit func checkDevice
    when socket Connect, check Device before accept receive data from client

--- sockets/function.js
+++ sockets/function.js
@@ -28,6 +28,14 @@ async function checkSocketToken(document) {
         return false;
     }
 }
+async function checkDevice(deviceID) {
+    try {
+        const device = await DeviceService.getDevice(deviceID);
+        return device;
+    } catch (error) {
+        return error;
+    }
+}
 
 function socketFunction(socket) {
     console.log('Device connected', socket.id);
@@ -36,32 +44,24 @@ function socketFunction(socket) {
         console.log('Device disconnected', socket.id);
     });
     socket.on('send-data-from-client', (document) => {
-        checkSocketToken(document).then((result) => {
-            if (result === false) {
+        checkDevice(document.device).then((result, error) => {
+            if (error) {
                 socket.emit('return-from-server', 'Auth failed');
                 socket.disconnect();
-            } else {
-                saveData(socket, document).then((data, error) => {
-                    if (error) {
-                        socket
-                            .to(socket.id.toString())
-                            .emit('return-from-server', 'Save data failed !');
-                    } else {
-                        socket.to(socket.id.toString()).emit('return-from-server', data);
-                    }
-                });
             }
+            saveData(socket, document).then((data, error) => {
+                if (error) {
+                    socket
+                        .to(socket.id.toString())
+                        .emit('return-from-server', 'Save data failed !');
+                } else {
+                    socket.to(socket.id.toString()).emit('return-from-server', data);
+                }
+            });
         });
     });
 }
 
-function Function(socket) {
-    console.log('Device connected', socket.id);
-    socket.on('disconnect', () => {
-        console.log('Device disconnected', socket.id);
-    });
-    socket.on('auth-device', (device) => []);
-}
 module.exports = {
     socketFunction,
 };
