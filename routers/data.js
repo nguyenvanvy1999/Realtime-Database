1cbe3820047773d77311a9865d9222251615792f
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-02-06 07:53:50 +0700

    fix bugs

--- routers/data.js
+++ routers/data.js
@@ -13,32 +13,29 @@ module.exports = () => {
         .get(
             JoiValidate.user.token,
             JwtMiddleware.isAuth,
-            DataController.getDataByUser,
-            handleError
+            DataController.getDataByUser
         );
     router
         .route('/get-data-device')
         .get(
             JoiValidate.device.deviceID,
             JwtMiddleware.isAuth,
-            DataController.getDataByDevice,
-            handleError
+            DataController.getDataByDevice
         );
     router
         .route('/delete-data-user')
         .delete(
             JoiValidate.user.token,
             JwtMiddleware.isAuth,
-            DataController.deleteDataByUser,
-            handleError
+            DataController.deleteDataByUser
         );
     router
         .route('/delete-data-device')
         .delete(
             JoiValidate.device.deviceID,
             JwtMiddleware.isAuth,
-            DataController.deleteDataByDevice,
-            handleError
+            DataController.deleteDataByDevice
         );
+    router.use(handleError);
     return router;
 };
--- routers/file.js
+++ routers/file.js
@@ -8,15 +8,14 @@ const multer = require('multer');
 module.exports = () => {
     router
         .route('/upload/single')
-        .post(JwtMiddleware.isAuth, UploadController.upload, handleError);
+        .post(JwtMiddleware.isAuth, UploadController.upload);
     router
         .route('/upload/multi')
-        .post(JwtMiddleware.isAuth, UploadController.upload, handleError); //FIXME:fix  upload many files
+        .post(JwtMiddleware.isAuth, UploadController.upload); //FIXME:fix  upload many files
     router
         .route('/list-file')
-        .get(multer().none(), UploadController.getListFiles, handleError); // FIXME: add role auth here
-    router
-        .route('/download')
-        .get(multer().none(), UploadController.download, handleError); // FIXME: add role auth here
+        .get(multer().none(), UploadController.getListFiles); // FIXME: add role auth here
+    router.route('/download').get(multer().none(), UploadController.download); // FIXME: add role auth here
+    router.use(handleError);
     return router;
 };
