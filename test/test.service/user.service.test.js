2f6bd7f3e1aa669d2b5b9d483bc18da20d6ef8c3
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-02-13 17:02:40 +0700

    add some test cases for user service

--- test/test.service/user.service.test.js
+++ test/test.service/user.service.test.js
@@ -1,19 +1,20 @@
-const { expect } = require('chai'),
+const { expect, should } = require('chai'),
     assert = require('assert'),
     mongoose = require('mongoose'),
-    UserService = require('../../services/user'), { seedAdmin, seedUser } = require('../config/seed'), { connect, clearData, closeDatabase } = require('../config/database'),
+    UserService = require('../../services/user'),
+    User = require('../../models/user'), { seedAdmin, seedUser } = require('../config/seed'), { connect, clearData, closeDatabase } = require('../config/database'),
     bcryptHelper = require('../../helpers/bcrypt'), { APIError } = require('../../helpers/error');
 
 before(async() => await connect());
-// let user, admin;
-// beforeEach(async() => {
-//     user = await seedUser;
-//     admin = seedAdmin;
-// });
+let user, admin;
+beforeEach(async() => {
+    user = await seedUser();
+    admin = await seedAdmin();
+});
 
 describe('UserService', () => {
     describe('new user and insert to DB', () => {
-        it('should save user to DB', async() => {
+        it('true => save user to DB', async() => {
             const mockUser = {
                 email: 'test@example.com',
                 username: 'test',
@@ -32,34 +33,59 @@ describe('UserService', () => {
             ).equal(true);
             expect(user.isActive).equal(false);
         });
-        it('ignore some value => throw new APIError', async() => {
-            assert.throws(
-                () => UserService.newUser(null),
-                APIError,
-                "Cannot read property 'email' of null"
+        it('ignore value when new user => throw new APIError', async() => {
+            assert.throws(() => UserService.newUser(null), APIError);
+        });
+        it('ignore value when save => throw new APIError', async() => {
+            await assert.rejects(
+                async() => await UserService.insert(null),
+                APIError
             );
         });
     });
-    describe('edit user', () => {
-        it('should edit user', async() => {
-            const seedUser = await seedAdmin();
+    describe('edit user by email', () => {
+        it('true => edit user', async() => {
             const mockUser = {
                 username: 'edit',
                 password: '1234',
             };
             const user = await UserService.editUser(
-                seedUser.email,
+                admin.email,
                 mockUser.username,
                 mockUser.password
             );
             expect([user.email, user.username, user.password]).eql([
-                seedUser.email,
+                admin.email,
                 mockUser.username,
                 mockUser.password,
             ]);
         });
+        it('no user found => return null', async() => {
+            const result = await UserService.deleteUserByEmail('test.mail');
+            expect(result).to.equal(null);
+        });
+    });
+    describe('delete user by email', () => {
+        it('true => delete user', async() => {
+            await UserService.deleteUserByEmail(admin.email);
+            const result = await User.find();
+            expect(result).to.have.length(1); //before have 2 user (user and admin)
+        });
+        it('no user found => return an null', async() => {
+            const result = await UserService.deleteUserByEmail('test.mail');
+            expect(result).to.equal(null);
+        });
+    });
+    describe('get user by email', () => {
+        it('true => return user', async() => {
+            const result = await UserService.getUserByEmail(admin.email);
+            expect(JSON.stringify(result)).to.eql(JSON.stringify(admin));
+        });
+        it('no user found => return null', async() => {
+            const result = await UserService.getUserByEmail(123);
+            expect(result).to.equal(null);
+        });
     });
 });
-
 afterEach(async() => await clearData());
 after(async() => await closeDatabase());
