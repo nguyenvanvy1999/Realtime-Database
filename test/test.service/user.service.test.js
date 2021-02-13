eadff281e25afcfb30988287281e772de0b9aac5
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-02-13 07:20:23 +0700

    add test case editUser

--- test/test.service/user.service.test.js
+++ test/test.service/user.service.test.js
@@ -1,17 +1,18 @@
-const { expect, assert } = require('chai'), { connect, clearData, closeDatabase } = require('../config/database'),
-    UserService = require('../../services/user'), { seedAdmin, seedUser } = require('../config/seed'),
+const { expect } = require('chai'),
+    assert = require('assert'),
     mongoose = require('mongoose'),
+    UserService = require('../../services/user'), { seedAdmin, seedUser } = require('../config/seed'), { connect, clearData, closeDatabase } = require('../config/database'),
     bcryptHelper = require('../../helpers/bcrypt'), { APIError } = require('../../helpers/error');
 
 before(async() => await connect());
 // let user, admin;
-// // beforeEach(async() => {
-// //     user = await seedUser;
-// //     admin = seedAdmin;
-// // });
+// beforeEach(async() => {
+//     user = await seedUser;
+//     admin = seedAdmin;
+// });
 
 describe('UserService', () => {
-    describe('newUser', () => {
+    describe('new user and insert to DB', () => {
         it('should save user to DB', async() => {
             const mockUser = {
                 email: 'test@example.com',
@@ -31,13 +32,31 @@ describe('UserService', () => {
             ).equal(true);
             expect(user.isActive).equal(false);
         });
-        it('should throw new APIError', async() => {
-            const mockUser = null;
+        it('ignore some value => throw new APIError', async() => {
             assert.throws(
-                () => UserService.newUser(mockUser),
+                () => UserService.newUser(null),
                 APIError,
                 "Cannot read property 'email' of null"
             );
+        });
+    });
+    describe('edit user', () => {
+        it('should edit user', async() => {
+            const seedUser = await seedAdmin();
+            const mockUser = {
+                username: 'edit',
+                password: '1234',
+            };
+            const user = await UserService.editUser(
+                seedUser.email,
+                mockUser.username,
+                mockUser.password
+            );
+            expect([user.email, user.username, user.password]).eql([
+                seedUser.email,
+                mockUser.username,
+                mockUser.password,
+            ]);
         });
     });
 });
